170363a338c7d27e6a3e8eae8a4432328357ad3e
MuddyWinds <muddywinds@gmail.com>
2022-02-11 23:58:35 +0800

    Added promo section

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -16,7 +16,7 @@ function Sidebar() {
         <NavItemsContainer>
             {navItems.map((item, index) => (
                 <NavItem key={index} onClick={() => setActiveIcon(item.title)}>
-                    <NavIcon style={{ color: item.title == activeIcon && '#3773f5 '}}>{item.icon}</NavIcon>
+                    <NavIcon style={{color: item.title == activeIcon && '#3773f5 '}}>{item.icon}</NavIcon>
                     <NavTitle>{item.title}</NavTitle>
                 </NavItem>
             ))}
